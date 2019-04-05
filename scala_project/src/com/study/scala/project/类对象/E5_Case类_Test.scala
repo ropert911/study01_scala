4c59e8f3cabb781da319a474668659323092aabe
root <254323626@qq.com>
2019-04-05 10:34:03 +0800

    修改示例

---
+++ scala_project/src/com/study/scala/project/类对象/E5_Case类_Test.scala
@@ -0,0 +1,32 @@
+package com.study.scala.project.类对象
+
+
+object E5_Case类_Test {
+
+  trait SuperTrait
+
+  case class CaseClass1(a: Int, b: Int) extends SuperTrait
+
+  case class CaseClass2(a: Int) extends SuperTrait // Case class
+  case object CaseObject extends SuperTrait // Case object
+
+  //case类默认是不可变的;所有参数默认使用public和immutable修辞符
+  //所以纯数据处理常用
+  case class CaseClass(a: Int, b: Int)
+
+  def main(args: Array[String]) {
+    callCase(CaseClass1(10, 10))
+    callCase(CaseClass2(10))
+    callCase(CaseObject)
+
+    var c = CaseClass(10, 10) // 不使用new关键字实例化对象
+    println("a = " + c.a) // Accessing elements of case class
+    println("b = " + c.b)
+  }
+
+  def callCase(f: SuperTrait) = f match {
+    case CaseClass1(f, g) => println("a = " + f + " b =" + g)
+    case CaseClass2(f) => println("a = " + f)
+    case CaseObject => println("No Argument")
+  }
+}
---
+++ scala_project/src/com/study/scala/project/逻辑控制/异常处理.scala
@@ -0,0 +1,47 @@
+package com.study.scala.project.逻辑控制
+
+/**
+  * Created by xq on 2019/4/5.
+  */
+object 异常处理 {
+
+  class ExceptionExample {
+    def divide(a: Int, b: Int) = {
+      try {
+        a / b
+        var arr = Array(1, 2)
+        arr(10)
+      } catch {
+        case e: ArithmeticException => println(e)
+        case ex: Exception => println(ex)
+        case th: Throwable => println("found a unknown exception" + th)
+      }
+      finally {
+        println("Finaly block always executes")
+      }
+
+      println("Rest of the code is executing...")
+    }
+
+    //抛出异常
+    def validate(age: Int) = {
+      if (age < 18)
+        throw new ArithmeticException("You are not eligible")
+      else println("You are eligible")
+    }
+
+    //throws关键字来声明异常。可以使用方法定义声明异常, 它向调用者函数提供了此方法可能引发此异常的信息。
+    // 它有助于调用函数处理并将该代码包含在try-catch块中，以避免程序异常终止
+    @throws(classOf[NumberFormatException])
+    def validate()={
+      "abc".toInt
+    }
+  }
+
+  def main(args: Array[String]) {
+    var e = new ExceptionExample()
+    e.divide(100, 10)
+  }
+}
+
+
