a0fa8c22227d0da7c9a53b0026ad94c4f40fea7e
ropert911 <254323626@qq.com>
2019-11-05 15:43:48 +0800

    代码调整

--- scala_project/src/com/study/scala/project/String_Test.scala
+++ scala_project/src/com/study/scala/project/String_Test.scala
@@ -13,15 +13,19 @@ object String_Test {
 
     val name = "James"
     println(s"Hello, $name")
-    println(s"1 + 1 = ${1 + 1}")                        //s插值器：允许使用变量
+    println(s"1 + 1 = ${1 + 1}") //s插值器：允许使用变量
     val height = 1.9d
-    println(f"$name%s is $height%2.2f meters tall")   //f插值器：格式化
+    println(f"$name%s is $height%2.2f meters tall") //f插值器：格式化
 
-    val a = "3009"
-    val b = "3001"
-    val c = "3010"
-    if (b.compareTo(a)<=0 && c.compareTo(a)>=0){
-      println("aaaaaaaaaaa")
-    }
+
+    val str111 = "3000000500001804^000080029C09E987^97^98^99^20191105143115^5.5^null^null^null^null^null^null^null^null^1000.0^3000.0^1000.0^3008^20191105"
+    val params = str111.split('^')
+    params.foreach(item =>
+      println("aaa:" + item)
+    )
+    val params2 = str111.split("^")
+    params2.foreach(item =>
+      println("bbb:" + item)
+    )
   }
 }
